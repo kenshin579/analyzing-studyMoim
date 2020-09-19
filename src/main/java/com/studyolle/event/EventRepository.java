565ad88c0c263e39587f30aa1e6bdfb6babeebcf
insookim0702 <insookim0702@gmail.com>
2020-09-20 02:36:15 +0900

    모임 @EntityGraph jpa메서드 추가

--- src/main/java/com/studyolle/event/EventRepository.java
+++ src/main/java/com/studyolle/event/EventRepository.java
@@ -2,6 +2,7 @@ package com.studyolle.event;
 
 import com.studyolle.domain.Event;
 import com.studyolle.domain.Study;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -9,5 +10,6 @@ import java.util.List;
 
 @Transactional(readOnly = true)
 public interface EventRepository extends JpaRepository<Event, Long> {
-    List<Event> findByStudy(Study study);
+    @EntityGraph(value = "Event.withEnrollment", type = EntityGraph.EntityGraphType.LOAD)
+    List<Event> findByStudyOrderByStartDateTime(Study study);
 }
