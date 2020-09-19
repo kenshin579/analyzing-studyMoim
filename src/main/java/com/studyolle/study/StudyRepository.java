2df357429c9733976ea4bab306ca571e97beaf6e
insookim0702 <insookim0702@gmail.com>
2020-09-20 02:38:42 +0900

    스터디 관련

--- src/main/java/com/studyolle/study/StudyRepository.java
+++ src/main/java/com/studyolle/study/StudyRepository.java
@@ -19,7 +19,7 @@ public interface StudyRepository extends JpaRepository<Study, Long> {
     @EntityGraph(value = "Study.withZonesAndManagers", type= EntityGraph.EntityGraphType.FETCH)
     Study findAccountWithZonesByPath(String path);
 
-    @EntityGraph(value = "Study.withMembers", type = EntityGraph.EntityGraphType.FETCH)
+    @EntityGraph(value = "Study.withManagers", type = EntityGraph.EntityGraphType.FETCH)
     Study findStudyWithManagersByPath(String path);
 
     @EntityGraph(value = "Study.withMembers", type = EntityGraph.EntityGraphType.FETCH)
