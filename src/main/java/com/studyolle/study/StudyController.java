bfcbcb5c642a47a3ae24ae64460179942b4571b7
insookim0702 <insookim0702@gmail.com>
2020-09-24 23:38:47 +0900

    변수명 변경 및 메서드 수정

--- src/main/java/com/studyolle/study/StudyController.java
+++ src/main/java/com/studyolle/study/StudyController.java
@@ -65,14 +65,14 @@ public class StudyController {
 
     @GetMapping("/study/{path}/members")
     public String members(@PathVariable String path, Model model){
-        Study byPath = studyService.getStudy(path);
-        model.addAttribute(byPath);
+        Study study = studyService.getStudy(path);
+        model.addAttribute(study);
         return "study/members";
     }
 
     @GetMapping("/study/{path}/events")
     public String eventView(@CurrentUser Account account, @PathVariable String path, Model model){
-        Study study = studyService.getStudyToUpdateStatus(account, path);
+        Study study = studyService.getStudy(path);
         model.addAttribute(study);
         Map<String, List<Event>> events = eventService.getEvents(study);
         model.addAttribute("newEvents", events.get("newEvents"));
