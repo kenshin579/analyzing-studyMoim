194a9db15ab28c7df54a23ce297e4cbd17c43364
insookim0702 <insookim0702@gmail.com>
2020-09-26 23:39:39 +0900

    enrollment데이터 존재 확인 메서드 추가

--- src/main/java/com/studyolle/event/EnrollmentRepository.java
+++ src/main/java/com/studyolle/event/EnrollmentRepository.java
@@ -7,4 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EnrollmentRepository extends JpaRepository<Enrollment, Long> {
     Enrollment findByEventAndAccount(Event event, Account account);
+
+    boolean existsByEventAndAccount(Event event, Account account);
+
 }
