34c8cdbd7496a314b2af1799d2a36d5042464c22
insookim0702 <insookim0702@gmail.com>
2020-09-30 00:06:34 +0900

    add getEnrollment()

--- src/main/java/com/studyolle/event/EnrollmentService.java
+++ src/main/java/com/studyolle/event/EnrollmentService.java
@@ -7,41 +7,17 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
-import java.util.List;
-@Transactional
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 @Service
 public class EnrollmentService {
     private final EnrollmentRepository enrollmentRepository;
 
-    public void addFCFSEnrollment(Event event, Account account, boolean joinStat) {
-        if(!enrollmentRepository.existsByEventAndAccount(event, account)){
-            Enrollment enrollment = new Enrollment();
-            enrollment.setAccount(account);
-            enrollment.setEnrolledAt(LocalDateTime.now());
-            enrollment.setEvent(event);
-            enrollment.setAccepted(joinStat);
-            event.addEnrollment(enrollment);
-            enrollmentRepository.save(enrollment);
-        }
-    }
-    public boolean alreadyEnroll(Event event, Account account){
-        return event.getEnrollments().contains(account);
-    }
-
-    public void removeFCFSEnrollment(Event event, Enrollment enrollment, List<Enrollment> enrollments) {
-        event.removeEnrollment(enrollment);
-        this.updateEnrollStat(enrollments, event.getLimitOfEnrollments());
-    }
-
-    public void updateEnrollStat(List<Enrollment> enrollments, int limitOfEnrollment) {
-        for(int i =0; i< limitOfEnrollment;i++){
-            enrollments.get(i).setAccepted(true);
-        }
-    }
-
     public Enrollment getEnrollment(Event event, Account account) {
         return enrollmentRepository.findByEventAndAccount(event, account);
+    }
+
+    public Enrollment getEnrollement(Long enrollmentId) {
+        return enrollmentRepository.findById(enrollmentId).orElseThrow();
     }
 }
