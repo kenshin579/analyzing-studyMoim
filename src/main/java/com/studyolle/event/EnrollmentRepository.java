b45a883ff711f5dc8fa94409d984133448181b3a
insookim0702 <insookim0702@gmail.com>
2020-09-30 00:07:05 +0900

    add findByEvent()

--- src/main/java/com/studyolle/event/EnrollmentRepository.java
+++ src/main/java/com/studyolle/event/EnrollmentRepository.java
@@ -5,9 +5,12 @@ import com.studyolle.domain.Enrollment;
 import com.studyolle.domain.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EnrollmentRepository extends JpaRepository<Enrollment, Long> {
     Enrollment findByEventAndAccount(Event event, Account account);
 
     boolean existsByEventAndAccount(Event event, Account account);
 
+    List<Enrollment> findByEvent(Event event);
 }
