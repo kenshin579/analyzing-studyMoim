2d05e2bd50cbb9b9c68f8a70ebacbf4312d669fb
insookim0702 <insookim0702@gmail.com>
2020-10-10 23:54:31 +0900

    스터디 공개 시, [알림 발송] event 발생 기능 추가

--- src/main/java/com/studyolle/modules/study/event/StudyEventListener.java
+++ src/main/java/com/studyolle/modules/study/event/StudyEventListener.java
@@ -1,21 +1,83 @@
 package com.studyolle.modules.study.event;
 
+import com.studyolle.infra.config.AppProperties;
+import com.studyolle.infra.mail.EmailMessage;
+import com.studyolle.infra.mail.EmailService;
+import com.studyolle.modules.account.Account;
+import com.studyolle.modules.account.AccountPredicate;
+import com.studyolle.modules.account.AccountRepository;
+import com.studyolle.modules.notification.Notification;
+import com.studyolle.modules.notification.NotificationRepository;
+import com.studyolle.modules.notification.NotificationType;
 import com.studyolle.modules.study.Study;
+import com.studyolle.modules.study.StudyRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import java.time.LocalDateTime;
+
 
 @Slf4j
 @Async
-@Transactional(readOnly = true)
+@Transactional
 @Component
+@RequiredArgsConstructor
 public class StudyEventListener {
+    private final StudyRepository studyRepository;
+    private final AccountRepository accountRepository;
+    private final EmailService emailService;
+    private final TemplateEngine templateEngine;
+    private final AppProperties appProperties;
+    private final NotificationRepository notificationRepository;
+
     @EventListener
     public void handleStudyCreatedEvent(StudyCreatedEvent studyCreatedEvent){
-        Study study = studyCreatedEvent.getStudy();
-        log.info(study.getTitle() +" is created");
-        throw new RuntimeException();
+        Study study =studyRepository.findStudyWithZoneAndTagById(studyCreatedEvent.getStudy().getId());
+        Iterable<Account> accounts = accountRepository.findAll(AccountPredicate.findByTagsAndZones(study.getTags(), study.getZones()));
+
+        accounts.forEach(account -> {
+                if(account.isAlarmStudyCreationToEmail()){
+                    sendStudyCreatedEmail(study, account);
+                }
+                if (account.isAlarmStudyCreationToWeb()){
+                    saveStudyCreateNotification(study, account);
+
+                }
+        });
+    }
+
+    private void saveStudyCreateNotification(Study study, Account account) {
+        Notification notification = new Notification();
+        notification.setTitle(study.getTitle());
+        notification.setLink("/study/"+study.getPath());
+        notification.setCreatedLocalDateTime(LocalDateTime.now());
+        notification.setMessage(study.getShortDescription());
+        notification.setChecked(false);
+        notification.setAccount(account);
+        notification.setNotificationType(NotificationType.STUDY_CREATED);
+        notificationRepository.save(notification);
+    }
+
+    private void sendStudyCreatedEmail(Study study, Account account) {
+        Context context = new Context();
+        context.setVariable("nickname", account.getNickname());
+        context.setVariable("link", "/study/"+study.getPath());
+        context.setVariable("linkName",study.getTitle());
+        context.setVariable("content",study.getShortDescription());
+        context.setVariable("message", "새로운 스터디가 생겼습니다.");
+        context.setVariable("host", appProperties.getHost());
+        String message = templateEngine.process("mail/studyCreatedMessage", context);
+        EmailMessage emailMessage = EmailMessage.builder()
+                .subject("스터디모임, '"+study.getTitle()+"'스터디가 생성되었습니다.")
+                .to(account.getEmail())
+                .message(message)
+                .build();
+        emailService.sendEmail(emailMessage);
     }
 }
